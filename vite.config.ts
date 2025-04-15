7252a6387654c795f0b6b184248da928bdc02f8a
Gypsophila <futur_wxc@163.com>
2025-04-15 09:56:25 +0800

    build: 添加路径别名配置以简化导入路径

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 import UnoCSS from 'unocss/vite';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), UnoCSS()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
 });
