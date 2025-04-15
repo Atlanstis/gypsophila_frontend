8c8c8b0cf29ed51f4cffa23a9b27d738513a5c68
Gypsophila <futur_wxc@163.com>
2025-04-15 10:52:09 +0800

    feat: 添加 Naive UI
    添加 Naive UI 作为依赖，并在 Vite 配置中集成组件自动导入功能。

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,28 @@
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 import UnoCSS from 'unocss/vite';
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), UnoCSS()],
+  plugins: [
+    vue(),
+    UnoCSS(),
+    Components({
+      // 配置组件自动导入的目录
+      dirs: ['src/components'],
+      // 组件的有效文件扩展名
+      extensions: ['vue'],
+      // 配置文件生成位置
+      dts: 'src/types/components.d.ts',
+      // 配置组件自动导入的类型
+      types: [{ from: 'vue-router', names: ['RouterLink', 'RouterView'] }],
+      // 添加 Naive UI 解析器
+      resolvers: [NaiveUiResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
